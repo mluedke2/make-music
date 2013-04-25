978cc66e0ec3636700504b59b2211043905c5db5
mluedke2 <mluedke2@gmail.com>
2013-04-25 12:21:42 -0500

    Initial Commit

---
+++ Make Music/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Make Music
+//
+//  Created by Matt Luedke on 4/25/13.
+//  Copyright (c) 2013 Matt Luedke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
